be030bcd8d5d57a4fe80c93b00af697cfa73031a
Chin <cisaache@mines.edu>
2012-05-10 00:29:49 -0600

    wont seed the db for some reason still... giving up for real

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-board.create(:name => 'HarryJamesPotter', :score => 9)
-board.create(:name => 'ZedCthulhu', :score => 3)
-board.create(:name => 'NearlyDied', :score => 2)
+boards.create(:name => 'HarryJamesPotter', :score => 9)
+boards.create(:name => 'ZedCthulhu', :score => 3)
+boards.create(:name => 'NearlyDied', :score => 2)
